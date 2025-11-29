f99466131049e546f020c4cfe7840c493d24d8cf
fawwerty <kwafonathanielsenior@gmail.com>
2025-11-29 15:17:19 +0000

    Build: PostCSS/Tailwind setup and CSS fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Bankly-banking-website/', // Important for GitHub Pages
   server: {
     port: 3000,
     host: true
